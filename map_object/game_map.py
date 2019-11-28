bb256092952813a6ad8eb119a850caff57a57ae8
Tentoshka <lnvdmitry@otlook.com>
2019-11-28 17:39:12 +0300

    Generate Map

--- map_object/game_map.py
+++ map_object/game_map.py
@@ -27,31 +27,31 @@ class GameMap:
             x = randint(0, map_width - w - 1)
             y = randint(0, map_height - h - 1)
 
-        new_room = Rect(x, y, w, h)
+            new_room = Rect(x, y, w, h)
 
-        for other_room in rooms:
-            if new_room.intersect(other_room):
-                break
-        else:
-            self.create_room(new_room)
-
-            (new_x, new_y) = new_room.center()
-
-            if num_rooms == 0:
-                player.x = new_x
-                player.y = new_y
+            for other_room in rooms:
+                if new_room.intersect(other_room):
+                    break
             else:
-                (prev_x, prev_y) = rooms[num_rooms - 1].center()
+                self.create_room(new_room)
 
-                if randint(0, 1) == 1:
-                    self.create_h_tunnel(prev_x, new_x, prev_y)
-                    self.create_v_tunnel(prev_y, new_y, new_x)
+                (new_x, new_y) = new_room.center()
+
+                if num_rooms == 0:
+                    player.x = new_x
+                    player.y = new_y
                 else:
-                    self.create_v_tunnel(prev_y, new_y, prev_x)
-                    self.create_h_tunnel(prev_x, new_x, new_y)
+                    (prev_x, prev_y) = rooms[num_rooms - 1].center()
 
-            rooms.append(new_room)
-            num_rooms += 1
+                    if randint(0, 1) == 1:
+                        self.create_h_tunnel(prev_x, new_x, prev_y)
+                        self.create_v_tunnel(prev_y, new_y, new_x)
+                    else:
+                        self.create_v_tunnel(prev_y, new_y, prev_x)
+                        self.create_h_tunnel(prev_x, new_x, new_y)
+
+                rooms.append(new_room)
+                num_rooms += 1
 
     def create_room(self, room):
         for x in range(room.x1 + 1, room.x2):
@@ -65,7 +65,7 @@ class GameMap:
             self.tiles[x][y].block_sight = False
 
     def create_v_tunnel(self, y1, y2, x):
-        for x in range(min(y1, y2), max(y1, y2) + 1):
+        for y in range(min(y1, y2), max(y1, y2) + 1):
             self.tiles[x][y].blocked = False
             self.tiles[x][y].block_sight = False
 
